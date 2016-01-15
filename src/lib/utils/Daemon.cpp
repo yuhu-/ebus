9dfae95e91ad6aecd7d680a636c48d06ac57a279
Roland Jax <roland.jax@liwest.at>
2016-01-15 10:30:17 +0100

    more utils added.

---
+++ src/lib/utils/Daemon.cpp
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) Roland Jax 2012-2016 <roland.jax@liwest.at>
+ *
+ * This file is part of ebusgate.
+ *
+ * ebusgate is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ebusgate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ebusgate. If not, see http://www.gnu.org/licenses/.
+ */
+
+#include "Daemon.h"
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+using std::cerr;
+using std::endl;
+
+Daemon& Daemon::getInstance()
+{
+	static Daemon instance;
+	return (instance);
+}
+
+void Daemon::start(const char* pidfile)
+{
+	m_pidfile = pidfile;
+
+	pid_t pid;
+
+	// fork off the parent process
+	pid = fork();
+
+	if (pid < 0)
+	{
+		cerr << "daemon fork() failed." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// If we got a good PID, then we can exit the parent process
+	if (pid > 0)
+	{
+		// printf("Child process created: %d\n", pid);
+		exit(EXIT_SUCCESS);
+	}
+
+	// At this point we are executing as the child process
+
+	// Set file permissions 750
+	umask(S_IWGRP | S_IRWXO);
+
+	// Create a new SID for the child process and
+	// detach the process from the parent (normally a shell)
+	if (setsid() < 0)
+	{
+		cerr << "daemon setsid() failed." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// Change the current working directory. This prevents the current
+	// directory from being locked; hence not being able to remove it.
+	if (chdir("/tmp") < 0)
+	{  //DAEMON_WORKDIR
+		cerr << "daemon chdir() failed." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// Close stdin, stdout and stderr
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+
+	// write pidfile and try to lock it
+	m_pidfd = fopen(m_pidfile, "w+");
+
+	umask(S_IWGRP | S_IRWXO);
+
+	if (m_pidfd != NULL)
+	{
+		setbuf(m_pidfd, NULL);
+		if (lockf(fileno(m_pidfd), F_TLOCK, 0) < 0
+			|| fprintf(m_pidfd, "%d\n", getpid()) <= 0)
+		{
+			fclose(m_pidfd);
+			m_pidfd = NULL;
+		}
+	}
+
+	if (m_pidfd == NULL)
+	{
+		cerr << "can't open pid file: " << m_pidfile << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	m_status = true;
+}
+
+void Daemon::stop()
+{
+	if (m_pidfd != NULL)
+	{
+		if (fclose(m_pidfd) < 0) return;
+		remove(m_pidfile);
+	}
+}
---
+++ src/lib/utils/Daemon.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) Roland Jax 2012-2016 <roland.jax@liwest.at>
+ *
+ * This file is part of ebusgate.
+ *
+ * ebusgate is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ebusgate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ebusgate. If not, see http://www.gnu.org/licenses/.
+ */
+
+#ifndef UTILS_DAEMON_H
+#define UTILS_DAEMON_H
+
+#include <cstddef>
+#include <cstdio>
+
+class Daemon
+{
+
+public:
+	static Daemon& getInstance();
+
+	void start(const char* pidfile);
+
+	void stop();
+
+	bool status()
+	{
+		return (m_status);
+	}
+
+private:
+	Daemon()
+	{
+	}
+	Daemon(const Daemon&);
+	Daemon& operator=(const Daemon&);
+
+	bool m_status = false;
+
+	const char* m_pidfile = NULL;
+	FILE* m_pidfd = NULL;
+
+};
+
+#endif // UTILS_DAEMON_H
