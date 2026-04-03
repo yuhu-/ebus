badaba361d5e75b3f4f7647f09628f77d669caa8
Roland Jax <roland.jax@liwest.at>
2026-04-04 00:04:31 +0200

    Handler test fixed

--- legacy/Core/legacy_handler.cpp
+++ legacy/Core/legacy_handler.cpp
@@ -140,7 +140,7 @@ bool run_test(const TestCase& tc) {
   handler.setSourceAddress(tc.address);
 
   // Prepare test sequence from the provided hex string
-  std::string tmp = "aaaaaa" + tc.read_string + "aaaaaa";
+  std::string tmp = "aaaaaaaa" + tc.read_string + "aaaaaa";
   ebus::Sequence seq;
   seq.assign(ebus::to_vector(tmp));
 
