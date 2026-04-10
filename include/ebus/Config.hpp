5bcdaf680a1648151fd833b4317451f7615d9988
Roland Jax <roland.jax@liwest.at>
2026-04-10 21:41:55 +0200

    clean up

--- include/ebus/Config.hpp
+++ include/ebus/Config.hpp
@@ -5,23 +5,24 @@
 
 #pragma once
 
+#include <chrono>
 #include <cstdint>
 #include <string>
 
 namespace ebus {
 
 /**
- * Configuration that can be updated during runtime without restarting the hardware.
+ * Configuration that can be updated during runtime without restarting the
+ * hardware.
  */
 struct RuntimeConfig {
   uint8_t address = 0xff;
   uint16_t window = 4300;
   uint16_t offset = 80;
+  uint8_t lock_counter_max = 3;
   uint32_t syn_base_ms = 50;
   uint32_t syn_tolerance_ms = 5;
-  uint8_t lock_counter_max = 3;
   bool enable_syn = false;
-  // bool syn_deterministic = true;
 };
 
 /**
@@ -48,7 +49,7 @@ struct busConfig {
  */
 struct ebusConfig {
   RuntimeConfig runtime = {};
-  uint32_t clientTimeoutMs = 1000;
+  std::chrono::milliseconds clientTimeoutMs{500};
   busConfig bus = {};
 };
 
