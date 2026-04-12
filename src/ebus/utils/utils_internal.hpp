6641eb65469bc90146ff865cac7117ca5e94811e
Roland Jax <roland.jax@liwest.at>
2026-04-12 21:17:42 +0200

    utils_internal soure removed

--- src/ebus/utils/utils_internal.hpp
+++ src/ebus/utils/utils_internal.hpp
@@ -13,6 +13,18 @@ namespace ebus {
 /**
  * Returns the number of zero bits in a byte.
  */
-uint8_t countZeroBits(uint8_t byte);
+constexpr uint8_t countZeroBits(uint8_t byte) {
+#if defined(__GNUC__) || defined(__clang__)
+  return static_cast<uint8_t>(8 - __builtin_popcount(byte));
+#else
+  uint8_t count = 0;
+  for (int i = 0; i < 8; i++) {
+    if (!(byte & (1 << i))) {
+      count++;
+    }
+  }
+  return count;
+#endif
+}
 
 }  // namespace ebus
