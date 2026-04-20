b367017851e9e2b0d6f29afe7a12d02256f8709e
Roland Jax <roland.jax@liwest.at>
2026-04-20 21:30:39 +0200

    inline toJson added

--- include/ebus/device.hpp
+++ include/ebus/device.hpp
@@ -6,7 +6,10 @@
 #pragma once
 
 #include <cstdint>
+#include <iomanip>
+#include <sstream>
 #include <string>
+#include <vector>
 
 namespace ebus {
 
@@ -27,5 +30,43 @@ struct DeviceInfo {
     std::string product_code;   // 10-digit product identifier (digits 7-16)
   } vaillant;
 };
+
+/**
+ * Serializes DeviceInfo to a JSON object string.
+ */
+inline std::string toJson(const DeviceInfo& info) {
+  std::ostringstream oss;
+  oss << "{"
+      << "\"slave_address\":" << static_cast<int>(info.slave_address) << ","
+      << "\"manufacturer\":" << static_cast<int>(info.manufacturer) << ","
+      << "\"manufacturer_name\":\"" << info.manufacturer_name << "\","
+      << "\"unit_id\":\"" << info.unit_id << "\","
+      << "\"software_version\":\"" << info.software_version << "\","
+      << "\"hardware_version\":\"" << info.hardware_version << "\"";
+
+  if (!info.vaillant.serial_number.empty()) {
+    oss << ",\"vaillant\":{"
+        << "\"serial_number\":\"" << info.vaillant.serial_number << "\","
+        << "\"product_code\":\"" << info.vaillant.product_code << "\""
+        << "}";
+  }
+
+  oss << "}";
+  return oss.str();
+}
+
+/**
+ * Serializes a vector of DeviceInfo to a JSON array string.
+ */
+inline std::string toJson(const std::vector<DeviceInfo>& devices) {
+  std::ostringstream oss;
+  oss << "[";
+  for (size_t i = 0; i < devices.size(); ++i) {
+    if (i > 0) oss << ",";
+    oss << toJson(devices[i]);
+  }
+  oss << "]";
+  return oss.str();
+}
 
 }  // namespace ebus
